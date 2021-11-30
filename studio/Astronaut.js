30257e0ae6e8a53b412b722be3cede4a98100731
IsaacSol <soloisap001@gmail.com>
2021-11-29 19:53:01 -0600

    Mission complete

---
+++ studio/Astronaut.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.Astronaut = void 0;
+var Astronaut = /** @class */ (function () {
+    function Astronaut(massKg, myName) {
+        this.massKg = massKg;
+        this.name = myName;
+    }
+    return Astronaut;
+}());
+exports.Astronaut = Astronaut;
